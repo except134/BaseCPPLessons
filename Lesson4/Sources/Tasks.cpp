c575b4c786cec53ab9d6ae5478e50f0d0c2b381b
except134 <except134@gmail.com>
2021-06-04 07:50:43 +0300

    Добавлен урок 4

---
+++ Lesson4/Sources/Tasks.cpp
@@ -0,0 +1,21 @@
+#include "Lesson4PCH.h"
+
+void Tasks::Task1()
+{
+}
+
+void Tasks::Task2()
+{
+}
+
+void Tasks::Task3()
+{
+}
+
+void Tasks::Task4()
+{
+}
+
+void Tasks::Task5()
+{
+}
---
+++ Lesson4/Sources/Tasks.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Tasks
+{
+public:
+	void Task1();
+	void Task2();
+	void Task3();
+	void Task4();
+	void Task5();
+};
